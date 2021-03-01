bcd10542477270cb7ddc641d9bab24ce4d465425
rkbanala <ravikanthreddy1216@gmail.com>
2021-03-01 02:23:47 -0600

    03/01/2021 12.35

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import logging
+from loguru import logger
 import sys
 from config import DB_DETAILS
 from util import get_tables, load_db_details
@@ -10,19 +10,22 @@ def main():
     """Program takes at least one argument"""
     env = sys.argv[1]
     a_tables = sys.argv[2]
-    logging.basicConfig(filename='data-copier.info', encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.INFO)
-    logging.basicConfig(filename='data-copier.err', encoding='utf-8', level=logging.error())
+    #logging.basicConfig(filename='data-copier.info', encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.INFO)
+    #logging.basicConfig(filename='data-copier.err', encoding='utf-8', level=logging.error())
+    logger.add("data-copier.info", format="{time} {level} [{message}]", rotation="1 MB", retention="10 days", level="INFO")
     db_details = load_db_details(env)
     tables = get_tables('tables', a_tables)
     #table_name= 'departments'
     for table_name in tables['table_name']:
         #print(f'reading data for {table_name}')
-        logging.info(f'reading data for {table_name}')
+        #logging.info(f'reading data for {table_name}')
+        logger.info(f'reading data for {table_name}')
         data,column_names = read_table(db_details,table_name,1000)
 
         #print(data)
         #print(f'loading data for {table_name}')
-        logging.info('loading data for {table_name}')
+        #ogging.info('loading data for {table_name}')
+        logger.error(f'loading data for {table_name}')
         load_table(db_details,data,column_names,table_name)
 
     # for rec in data:
